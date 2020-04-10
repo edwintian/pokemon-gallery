3b3355dbf0b303291bac933bf8904f38ad052632
edwintian <rxchen87@gmail.com>
2020-04-10 17:37:35 +0800

    pokemon react practice

--- src/App.js
+++ src/App.js
@@ -2,13 +2,52 @@ import React from "react";
 import "./App.css";
 import pokemonData from "./pokemon/pokemon";
 
-const bulbasaur = pokemonData[0];
+function SetBackgroundColorForType(currType) {
+  let backgroundColor = "white";
+  if (currType === "Fire") {
+    backgroundColor = "#FF6600";
+  } else if (currType === "Grass") {
+    backgroundColor = "#99FF99";
+  } else if (currType === "Water") {
+    backgroundColor = "#99FFFF";
+  } else if (currType === "Poison") {
+    backgroundColor = "#9999FF";
+  } else if (currType === "Fairy") {
+    backgroundColor = "#FF99FF";
+  } else if (currType === "Ground") {
+    backgroundColor = "#996633";
+  } else if (currType === "Bug") {
+    backgroundColor = "#CC9900";
+  } else if (currType === "Electric") {
+    backgroundColor = "#FFFF00";
+  } else if (currType === "Flying") {
+    backgroundColor = "#CCCCFF";
+  }
+  return backgroundColor;
+}
 
 function PokemonCard({ pokemon }) {
   const { id, name, type, base } = pokemon;
   return (
-    <div>
-      <div>{name.english}</div>
+    <div className="pokemon">
+      <img
+        src={process.env.PUBLIC_URL + "/pokemonImage/" + id + ".png"}
+        alt=""
+      />
+      <div className="name">{name.english}</div>
+      {type.map(currType => (
+        <span
+          className="type"
+          style={{ backgroundColor: SetBackgroundColorForType(currType) }}
+        >
+          {currType}
+        </span>
+      ))}
+      {Object.keys(base).map(currKey => (
+        <div className="base">
+          {currKey}: {base[currKey]}
+        </div>
+      ))}
     </div>
   );
 }
@@ -16,7 +55,7 @@ function PokemonCard({ pokemon }) {
 function App() {
   return (
     <div className="App">
-      <PokemonCard pokemon={bulbasaur} />
+      {pokemonData.map(currPokemon => <PokemonCard pokemon={currPokemon} />)}
     </div>
   );
 }
